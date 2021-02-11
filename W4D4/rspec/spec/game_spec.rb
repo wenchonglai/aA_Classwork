e1ae5d036e87ffa685f37c3680629bc402a046b3
tpadgett <tim_padgett@yahoo.com>
2021-02-11 14:44:46 -0800

    Switch Turns

--- W4D4/rspec/spec/game_spec.rb
+++ W4D4/rspec/spec/game_spec.rb
@@ -1,6 +1,7 @@
 requrie "game"
 
 describe Game do
+
   describe "#initialize" do
     it "should accept a number indicating the number the disk" do
       expect{Game.new(6)}.to_not raise_error(ArgumentError)
@@ -13,21 +14,31 @@ describe Game do
     it "should generate three pegs" do
       expect(Game.new)
     end
-
-    it "should accept a number of disks from the user" do
-
-    end
   end
+
+  subject(:game) {Game.new(6)}
 
   describe "#move" do
-    it "should allow a player to stack a disk on a peg" do
-
+    double(:player, :get_input => [0, 1]) 
+    it "should recieve a player input" do
+      expect{game.move(player.get_input)}.to_not raise_error(ArgumentError)
     end
 
+    it "should correctly move the disc to the designated peg" do
+      expect(game.pegs).to eq([[], [1,2,3,4,5,6], []])
+      game.move(player.get_input)
+    end 
+
+    it "should "
+  end
+
+  describe "#play" do 
     it "should continue to allow moves until the game is won" do
+      expect(game).to receive(:play)
 
     end
   end
+
 
   describe "#won?" do
     it "should check if two of the pegs are empty" do
