e993fd6c1edf5b6a0384a6de22e983d2f3ac499b
Justin Cheasty <cheasty@gmail.com>
2021-02-17 14:41:52 -0800

    insert into tables debug

--- W5D3/aa-questions/import_db.sql
+++ W5D3/aa-questions/import_db.sql
@@ -56,3 +56,26 @@ CREATE TABLE question_likes(
   FOREIGN KEY (question_id) REFERENCES questions(id)
   -- like BOOLEAN REFERENCES questions(id)
 );
+
+INSERT INTO
+  users(f_name, l_name)
+VALUES (
+  'Donald', 'Trump'
+
+), ('Joe', 'Biden');
+
+INSERT INTO
+  questions(title, body, user_id)
+VALUES (
+  'Bigly?', 'Make App Academy Great Again', SELECT id FROM users WHERE l_name = 'Trump'
+);
+
+INSERT INTO
+  replies(user_id, question_id, parent_id, body)
+VALUES (
+  SELECT id FROM users WHERE l_name = 'Biden', 
+  SELECT id FROM questions WHERE title = 'Bigly?',
+  NULL,
+  'Just shut up, man..'
+);
+
