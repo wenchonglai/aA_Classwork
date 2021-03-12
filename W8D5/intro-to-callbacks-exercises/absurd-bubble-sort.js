9e8ca38ae7a393c0079451aabc7d86622bdbe019
theo m <theo@mantz.io>
2021-03-12 17:48:22 -0500

    Finished function calling

--- W8D5/intro-to-callbacks-exercises/absurd-bubble-sort.js
+++ W8D5/intro-to-callbacks-exercises/absurd-bubble-sort.js
@@ -4,8 +4,15 @@ const reader = readline.createInterface({
   output: process.stdout
 });
 
-function absurbBubbleSort(arr, sortCompletionCallback){
-  
+function absurdBubbleSort(arr, sortCompletionCallback){
+  function outerBubbleSortLoop(madeAnySwaps) {
+    if(madeAnySwaps) {
+      innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop);
+    } else {
+      sortCompletionCallback(arr);
+    }
+  }
+  outerBubbleSortLoop(true);
 }
 
 function askIfGreaterThan(el1, el2, callback){
@@ -17,3 +24,22 @@ function askIfGreaterThan(el1, el2, callback){
     }
   });
 }
+
+function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop){
+  if( i < arr.length - 1){
+    askIfGreaterThan(arr[i], arr[i + 1], (isGreaterThan) => {
+      if (isGreaterThan){
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        madeAnySwaps = true
+      }
+      innerBubbleSortLoop(arr, i + 1, madeAnySwaps, outerBubbleSortLoop);
+    });
+  } else {
+    outerBubbleSortLoop(madeAnySwaps);
+  }
+}
+
+absurdBubbleSort([3, 2, 1], function (arr) {
+  console.log("Sorted array: " + JSON.stringify(arr));
+  reader.close();
+});
