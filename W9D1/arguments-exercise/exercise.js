461b658175b45cac01024d5dc05b9131898b7ea5
byoung <youngben@earthlink.net>
2021-03-15 14:25:22 -0700

    complete first exercise

--- W9D1/arguments-exercise/exercise.js
+++ W9D1/arguments-exercise/exercise.js
@@ -7,10 +7,14 @@ function sum(...nums){
 // console.log(sum(1, 2, 3, 4, 5) === 15);
 
 Function.prototype.myBind = function(obj, ...args){
-  this.apply(obj, );
-  
-  
+  // this.apply(obj, );
+  return (...args1) => {
+    return this.apply(obj, [...args, ...args1])
+  }
 }
+
+
+
 
 class Cat {
   constructor(name) {
@@ -54,3 +58,39 @@ markov.says.myBind(pavlov, "meow")("Markov");
 // no bind time args (other than context), call time args are "meow" and "me"
 const notMarkovSays = markov.says.myBind(pavlov);
 notMarkovSays("meow", "me");
+// Pavlov says meow to me!
+// true
+
+
+
+
+
+
+
+Function.prototype.curry = function(length) {
+  let args = []
+  let self = this
+  return function _curry(num) {
+    args.push(num)
+    if (args.length === length) {
+      return self.apply(null, args)
+    } else {
+      return _curry
+    }
+  }
+}
+
+function sumThree(num1, num2, num3) {
+  return num1 + num2 + num3;
+}
+
+sumThree(4, 20, 6); // == 30
+
+// you'll write `Function#curry`!
+let f1 = sumThree.curry(3); // tells `f1` to wait until 3 arguments are given before running `sumThree`
+f1 = f1(4); // [Function]
+f1 = f1(20); // [Function]
+f1 = f1(6); // = 30
+
+// or more briefly:
+console.log(sumThree.curry(3)(4)(20)(6)); // == 30
