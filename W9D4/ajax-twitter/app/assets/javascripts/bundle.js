1d9c3b8580386d8cc2e2bafab3e5e452e4c4289e
mayagbarnes <maya8617@gmail.com>
2021-03-18 14:42:04 -0700

    Revise Part 1: Twitter

--- W9D4/ajax-twitter/app/assets/javascripts/bundle.js
+++ W9D4/ajax-twitter/app/assets/javascripts/bundle.js
@@ -12,7 +12,32 @@ class FollowToggle {
     this.button = $('.follow-toggle');
     this.userId = $el.data('userId');
     this.followState = $el.data('initialFollowState');
+
+    this.render();
   }
+
+  render() {
+    this.button.empty();
+    this.text( this.followState === "unfollowed" ? "Follow!" : "Unfollow!");
+  }
+
+  handleClick() {
+    this.button.on('click', _handleClick.bind(this) )
+  }
+}
+
+
+
+function _handleClick(e) {
+      e.preventDefault();
+
+      $.ajax({
+        url: `/users/${this.userId}/follow`,
+        method: ( this.followState ? 'DELETE' : 'POST' ),
+        dataType: 'JSON',
+        success: (res) => { this.button.data('initialFollowState', this.followState = !this.followState); this.render()}
+
+      })
 }
 
 module.exports = FollowToggle;
